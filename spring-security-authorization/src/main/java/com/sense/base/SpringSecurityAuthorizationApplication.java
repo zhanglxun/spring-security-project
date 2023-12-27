7a21b92fceb32fc3183f01e4901a5e534c6bf49b
jarvis2chang <jarvis2chang@gmail.com>
2023-12-27 11:10:25 +0800

    增加新项目oauth2

---
+++ spring-security-authorization/src/main/java/com/sense/base/SpringSecurityAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.sense.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAuthorizationApplication.class, args);
+    }
+
+}
